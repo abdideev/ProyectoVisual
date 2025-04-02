f8063370ca32bca5ee6cf88da475d445a0942086
Abdiel <av446034@uaeh.edu.mx>
2025-04-02 13:58:09 -0600

    dashboard interface

---
+++ Login.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GestionEmpresa
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            const string saludo = "Hola Mundo";
+            const string saludo2 = "Comprobar cambio de cuenta";
+            String ivan = "hola gays";
+        }
+    }
+}
